6a98904120c6912846621124d6f2734acad631ab
Kalpesh Mhaske <mhaskekalpesh3@gmail.com>
2025-01-21 12:53:42 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-<<<<<<< HEAD
  res.send('hello from Kalpesshhhhh...KING');
 =======
  res.send('hello from Kalpeshhh...Vaishhhhhhhhhh ');
