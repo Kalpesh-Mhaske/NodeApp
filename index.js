a7a4bf78976c6e7c330fe4937de29b04844e3f8a
Kalpesh Mhaske <mhaskekalpesh3@gmail.com>
2025-01-21 12:53:59 +0530

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();//Respond with "hello world" for requests that hit our root 
 app.get('/', function (req, res) {
  res.send('hello from Kalpesshhhhh...KING');
 =======
- res.send('hello from Kalpeshhh...Vaishhhhhhhhhh ');
+ res.send('hello from Kalpeshhh...Vaishhhhhh ');
 >>>>>>> 309d82c6a7a927862ef4531af624eddd7d56a121
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
